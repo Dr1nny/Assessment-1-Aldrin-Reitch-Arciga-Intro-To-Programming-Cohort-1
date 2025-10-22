68f53e05e03815c274a6ccc5f2a4d404e83fda47
Dr1nny <aldrin.reitcharciga@gmail.com>
2025-10-22 23:22:05 +0400

    Exercise9.py

--- Exercise9.py
+++ Exercise9.py
@@ -1,9 +1,10 @@
 
 def hello():
-    print("Hell")  # Fill in this blank to print "Hello" to the console
+    print("Hello")  # Fill in this blank to print "Hello" to the console
 
 def main():
     hello()  # Fill in this blank to call the hello() function
 
 if __name__ == "__main__":
+
     main() 
